6d5cea91c077bd7900f7f7e980b6e74d8d0354ad
riverpadilla <riverpadilla@msn.com>
2023-05-08 23:22:51 -0500

    Se crea codigo para busquedas en bing

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function search()
+{
+    console.log("Inicia Programa");
+    let form = document.createElement('form');
+    // form.action = 'https://google.com/search';
+    form.action = 'https://www.bing.com/results.asp';
+    form.method = 'GET';
+    // form.target = '_blank';
+
+    for (let i = 1; i <= 20; i++) {
+    text = "clima en mayo " + i;
+    form.innerHTML = '<input name="q" value=' + text + '>';
+    
+    // el formulario debe estar en el document para poder enviarlo
+    document.body.append(form);
+    
+    form.submit();
+    console.log(document.title);
+    window.history.back()
+    
+    }
+}
+
+
