4ae968c28011ea0c33326bc7042f45b7a87d77e4
riverpadilla <riverpadilla@msn.com>
2023-05-09 00:36:42 -0500

    Se modifica codigo para hacer las busquedas de forma que sean contabilizadas

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
 
 function search()
 {
-    console.log("Inicia Programa");
-    let form = document.createElement('form');
-    // form.action = 'https://google.com/search';
-    form.action = 'https://www.bing.com/results.asp';
-    form.method = 'GET';
-    // form.target = '_blank';
-
-    for (let i = 1; i <= 30; i++) {
-    text = "clima" + i;
-    form.innerHTML = '<input name="q" value=' + text + '>';
-    
-    // el formulario debe estar en el document para poder enviarlo
-    document.body.append(form);
-    
-    form.submit();
-    console.log(document.title);
-    window.history.back()
-    
+    let searchForm = document.getElementById('searchForm');
+    let searchInput = document.getElementById('search-input');
+    for (let i = 1; i <= 30; i++) 
+    {
+        texto = "clima de colombia " + i;
+        searchInput.setAttribute("value", texto);
+        searchForm.submit();
     }
 }
-
-
